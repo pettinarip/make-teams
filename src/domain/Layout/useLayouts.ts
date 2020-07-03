6169e7d9b3e9105081c68816cbdb0eb72fce0355
Pablo <pettinarip@gmail.com>
2020-07-03 22:05:38 +1000

    sort layouts by createdAt date

--- src/domain/Layout/useLayouts.ts
+++ src/domain/Layout/useLayouts.ts
@@ -7,10 +7,18 @@ export default function useLayouts(user: any) {
   const { data: layouts = [], ...rest } = useGetLayouts(user);
 
   const sortedLayouts = useMemo(() => {
-    return layouts.map(sortByPositionY);
+    // Sort layouts by `createdAt` date
+    const sorted = layouts.sort(sortByCreatedAt);
+
+    // Sort each layout positions by the Y position
+    return sorted.map(sortByPositionY);
   }, [layouts]);
 
   return { layouts: sortedLayouts, ...rest };
+}
+
+function sortByCreatedAt(a: ILayout, b: ILayout): number {
+  return a.createdAt.localeCompare(b.createdAt);
 }
 
 function sortByPositionY(layout: ILayout): ILayout {
