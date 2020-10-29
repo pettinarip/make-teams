654e3719b1db931ba3665209ffc8d7d591ed9a66
Pablo <pettinarip@gmail.com>
2020-10-29 20:32:29 -0300

    save color mode in localStorage

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ColorModeScript } from "@chakra-ui/core";
+
+export default class Document extends NextDocument {
+  static getInitialProps(ctx: DocumentContext) {
+    return NextDocument.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode="light" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
