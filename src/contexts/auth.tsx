f2afcb5350cd9828c0e1dd73b246f11114668396
Pablo <pettinarip@gmail.com>
2021-05-14 15:28:28 -0300

    remove unused auth methods on auth context

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,19 +1,13 @@
-import { useRouter } from "next/router";
 import { createContext, ReactNode, useContext } from "react";
 
 import useGetCurrentUser from "../dal/user/useGetCurrentUser";
-import useLogin, { IArgs } from "../dal/user/useLogin";
-import useLogout from "../dal/user/useLogout";
 import { IUser } from "../dal/user/types";
 import { FieldError } from "../graphql/API";
 
 interface IAuthContext {
-  isAuthenticated: boolean;
   isLoading: boolean;
   user?: IUser;
   error?: FieldError[] | null;
-  login: (args: IArgs) => void;
-  logout: () => void;
 }
 
 interface IAuthProviderProps {
@@ -21,35 +15,21 @@ interface IAuthProviderProps {
 }
 
 const initialContextState: IAuthContext = {
-  isAuthenticated: false,
   isLoading: false,
-  login: () => {},
-  logout: () => {},
 };
 
 const AuthContext = createContext<IAuthContext>(initialContextState);
 AuthContext.displayName = "AuthContext";
 
 export function AuthProvider({ children }: IAuthProviderProps) {
-  const router = useRouter();
   const { data: user, isLoading, error } = useGetCurrentUser();
-  const { mutateAsync: login } = useLogin();
-  const { mutateAsync: logout } = useLogout();
-
-  async function logoutWithRedirect() {
-    await logout();
-    router.push("/login");
-  }
 
   return (
     <AuthContext.Provider
       value={{
-        isAuthenticated: !!user,
         user,
         error,
-        login,
         isLoading,
-        logout: logoutWithRedirect,
       }}
     >
       {children}
--- src/dal/user/useGetCurrentUser.ts
+++ src/dal/user/useGetCurrentUser.ts
@@ -6,7 +6,7 @@ import { IUser } from "./types";
 
 export const QUERY_KEY = "CURRENT_USER";
 
-export default function useCurrentUser() {
+export default function useGetCurrentUser() {
   return useQuery<IUser | undefined, FieldError[]>(QUERY_KEY, async () => {
     const response = await sdk.Me();
     return response.me as IUser;
